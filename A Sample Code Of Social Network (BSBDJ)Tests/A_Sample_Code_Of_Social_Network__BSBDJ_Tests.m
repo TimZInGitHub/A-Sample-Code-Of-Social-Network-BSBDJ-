36e710a3f9c5290c935a5c7174ff2ebfee5932d8
TimZInGitHub <Tim@11234.local>
2015-11-23 21:45:09 +0800

    Initial Commit

---
+++ A Sample Code Of Social Network (BSBDJ)Tests/A_Sample_Code_Of_Social_Network__BSBDJ_Tests.m
@@ -0,0 +1,39 @@
+//
+//  A_Sample_Code_Of_Social_Network__BSBDJ_Tests.m
+//  A Sample Code Of Social Network (BSBDJ)Tests
+//
+//  Created by Tim.Z on 11/23/15.
+//  Copyright © 2015 Tim.Z. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface A_Sample_Code_Of_Social_Network__BSBDJ_Tests : XCTestCase
+
+@end
+
+@implementation A_Sample_Code_Of_Social_Network__BSBDJ_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
